fbbe1b69228882d6334eebb3389747cecb837759
Afmano <86706321+Afmano@users.noreply.github.com>
2025-09-17 16:51:02 +0300

    Morgan added.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || "3001";
 const pingURL = process.env.PING_URL || "https://telegram-gift-bot-3jp7.onrender.com/ping";
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   return res.status(200).json("Test route");
