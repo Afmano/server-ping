d0cacedb11b49e1f338244f992e398ecb9b59d4d
Afmano <86706321+Afmano@users.noreply.github.com>
2025-09-17 16:48:00 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || "3001";
+const pingURL = process.env.PING_URL || "https://telegram-gift-bot-3jp7.onrender.com/ping";
+
+app.get("/", (req, res) => {
+  return res.status(200).json("Test route");
+});
+
+app.get("/ping", (req, res) => {
+  return res.status(200).json("Ping successful");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  setTimeout(() => {
+    fetch(pingURL)
+  }, 500);
+});
